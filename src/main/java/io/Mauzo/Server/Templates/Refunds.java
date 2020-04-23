1db7364ab44d22f95631c6e5581c662be27c1a75
lluminar <60161182+lluminar@users.noreply.github.com>
2020-04-23 16:20:56 +0200

    Server - Añadidio atributos de refunds

--- src/main/java/io/Mauzo/Server/Templates/Refunds.java
+++ src/main/java/io/Mauzo/Server/Templates/Refunds.java
@@ -1,5 +1,43 @@
 package io.Mauzo.Server.Templates;
 
+import java.util.Date;
+
 public class Refunds {
-    
+
+    private int id;
+    private Date dateRefund;
+    private int userId;
+    private int saleId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDateRefund() {
+        return dateRefund;
+    }
+
+    public void setDateRefund(Date dateRefund) {
+        this.dateRefund = dateRefund;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getSaleId() {
+        return saleId;
+    }
+
+    public void setSaleId(int saleId) {
+        this.saleId = saleId;
+    }
 }
