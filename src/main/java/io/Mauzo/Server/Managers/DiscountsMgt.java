ebdc63947fd115bcb80553a84f98e10733f986c2
Ant04X <50796747+ant04x@users.noreply.github.com>
2020-05-04 20:57:09 +0200

    Server - Problemas de concurrencia.

--- src/main/java/io/Mauzo/Server/Managers/DiscountsMgt.java
+++ src/main/java/io/Mauzo/Server/Managers/DiscountsMgt.java
@@ -1,18 +1,27 @@
 package io.Mauzo.Server.Managers;
 
+import io.Mauzo.Server.Connections;
 import io.Mauzo.Server.ServerApp;
 import io.Mauzo.Server.Templates.Discount;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DiscountsMgt implements ManagersIntf<Discount> {
-    private static DiscountsMgt controller = null;
+    private final PreparedStatement addQuery;
+    private final PreparedStatement getIdQuery;
+    private final PreparedStatement getListQuery;
+    private final PreparedStatement modifyQuery;
+    private final PreparedStatement deleteQuery;
 
+    public DiscountsMgt(Connection conn) throws SQLException {
+        addQuery = conn.prepareStatement("INSERT INTO Discounts (id, codeDisc, descDisc, pricePrec)");
+        getIdQuery = conn.prepareStatement("SELECT * FROM Discounts WHERE id = ?;");
+        getListQuery = conn.prepareStatement("SELECT * FROM Discounts");
+        modifyQuery = conn.prepareStatement("UPDATE Discounts SET codeDisc = ?, descDisc = ?, pricePerc = ? WHILE id = ?;");
+        deleteQuery = conn.prepareStatement("DELETE FROM Discounts WHERE id = ?;");
+    }
 
     /**
      * Método para añadir descuentos a la base de datos.
@@ -22,22 +31,14 @@ public class DiscountsMgt implements ManagersIntf<Discount> {
      */
     @Override
     public void add(Discount discount) throws SQLException {
-        // Guardamos el puntero de conexión con la base de datos.
-        final Connection conn = ServerApp.getConnection();
+        // Asociamos los valores respecto a la sentencia sql.
+        addQuery.setInt(1, discount.getId());
+        addQuery.setString(2, discount.getCode());
+        addQuery.setString(3, discount.getDesc());
+        addQuery.setFloat(4, discount.getPrizeDisc());
 
-        // Preparamos la consulta sql.
-        // Las prepared statement no son de usar y tirar
-        try (PreparedStatement st = conn.prepareStatement("INSERT INTO Discounts (id, codeDisc, descDisc, pricePrec)")) {
-
-            // Asociamos los valores respecto a la sentencia sql.
-            st.setInt(1, discount.getId());
-            st.setString(2, discount.getCode());
-            st.setString(3, discount.getDesc());
-            st.setFloat(4, discount.getPrizeDisc());
-
-            // Ejecutamos la sentencia sql.
-            st.execute();
-        }
+        // Ejecutamos la sentencia sql.
+        addQuery.execute();
     }
 
     /**
@@ -53,30 +54,25 @@ public class DiscountsMgt implements ManagersIntf<Discount> {
     public Discount get(int id) throws SQLException, ManagerErrorException {
         Discount discount = null;
 
-        // Guardamos el puntero de conexión con la base de datos.
-        final Connection conn = ServerApp.getConnection();
+        // Asociamos los valores respecto a la sentencia sal.
+        getIdQuery.setInt(1, id);
 
-        // Preparamos le consulta sql.
-        try (PreparedStatement st = conn.prepareStatement("SELECT * FROM Discounts WHERE id = ?;")) {
-            // Asociamos los valores respecto a la sentencia sal.
-            st.setInt(1, id);
+        // Ejecutamos la sentencia sql y recuperamos lo que nos ha retornado.
+        try (ResultSet rs = getIdQuery.executeQuery()) {
+            discount = new Discount();
+            if (!(rs.isLast()))
+                while (rs.next()) {
+                    discount = new Discount();
 
-            // Ejecutamos la sentencia sql y recuperamos lo que nos ha retornado.
-            try (ResultSet rs = st.executeQuery()) {
-                discount = new Discount();
-                if (!(rs.isLast()))
-                    while (rs.next()) {
-                        discount = new Discount();
-
-                        discount.setId(rs.getInt("id"));
-                        discount.setCode(rs.getString("codeDisc"));
-                        discount.setDesc(rs.getString("descDisc"));
-                        discount.setPriceDisc(rs.getFloat("pricePerc"));
-                    }
-                else
-                    throw new ManagerErrorException("No se ha encontrado el descuento.");
-            }
+                    discount.setId(rs.getInt("id"));
+                    discount.setCode(rs.getString("codeDisc"));
+                    discount.setDesc(rs.getString("descDisc"));
+                    discount.setPriceDisc(rs.getFloat("pricePerc"));
+                }
+            else
+                throw new ManagerErrorException("No se ha encontrado el descuento.");
         }
+
         return discount;
     }
 
@@ -91,26 +87,21 @@ public class DiscountsMgt implements ManagersIntf<Discount> {
     public List<Discount> getList() throws SQLException {
         List<Discount> discountList = null;
 
-        // Guardamos el pontero de conexión con la base de datos.
-        final Connection conn = ServerApp.getConnection();
+        try (ResultSet rs = getListQuery.executeQuery()) {
+            discountList = new ArrayList<>();
 
-        // Lanzamos la consulta SQL y generamos la lista de descuentos.
-        try (PreparedStatement st = conn.prepareStatement("SELECT * FROM Discounts")) {
-            try (ResultSet rs = st.executeQuery()) {
-                discountList = new ArrayList<>();
+            while (rs.next()) {
+                Discount discount = new Discount();
 
-                while (rs.next()) {
-                    Discount discount = new Discount();
+                discount.setId(rs.getInt("id"));
+                discount.setCode(rs.getString("codeDisc"));
+                discount.setDesc(rs.getString("descDisc"));
+                discount.setPriceDisc(rs.getFloat("pricePerc"));
 
-                    discount.setId(rs.getInt("id"));
-                    discount.setCode(rs.getString("codeDisc"));
-                    discount.setDesc(rs.getString("descDisc"));
-                    discount.setPriceDisc(rs.getFloat("pricePerc"));
-
-                    discountList.add(discount);
-                }
+                discountList.add(discount);
             }
         }
+
         return discountList;
     }
 
@@ -123,49 +114,29 @@ public class DiscountsMgt implements ManagersIntf<Discount> {
      */
     @Override
     public void modify(Discount discount) throws SQLException, ManagerErrorException {
-        final Connection conn = ServerApp.getConnection();
+        // Asociamos los valores respecto a la sentencia sql.
+        modifyQuery.setString(1, discount.getCode());
+        modifyQuery.setString(2, discount.getDesc());
+        modifyQuery.setFloat(3, discount.getPrizeDisc());
 
-        // Preparamos la sentencia sql.
-        try (PreparedStatement st = conn.prepareStatement("UPDATE Discounts SET codeDisc = ?, descDisc = ?, pricePerc = ? WHILE id = ?;")) {
-            // Asociamos los valores respecto a la sentencia sql.
-            st.setString(1, discount.getCode());
-            st.setString(2, discount.getDesc());
-            st.setFloat(3, discount.getPrizeDisc());
-
-            // Ejecutamos la sentencia sql.
-            if (st.execute() == false)
-                throw new ManagerErrorException("No se ha encontrado el descuento durante la actualización del mismo.");
-        }
+        // Ejecutamos la sentencia sql.
+        if (modifyQuery.execute() == false)
+            throw new ManagerErrorException("No se ha encontrado el descuento durante la actualización del mismo.");
     }
 
     /**
      * Método para eliminar el usuario en la base de datos.
-     * 
+     *
      * @param discount El descuento encapsulado en un objeto.
      * @throws SQLException Excepción en la consulta SQL.
      * @throws ManagerErrorException Excepción dada al no encontrar el descuento solicitado.
      */
     @Override
     public void remove(Discount discount) throws SQLException, ManagerErrorException {
-        final Connection conn = ServerApp.getConnection();
-
-        // Preparamos la sentencia SQL.
-        try (PreparedStatement st = conn.prepareStatement("DELETE FROM Discounts WHERE id = ?;")) {
-            st.setInt(1, discount.getId());
-            // Ejecutamos la setencia sql.
-            if (st.execute() == false)
-                throw new ManagerErrorException("No se ha encontrado el descuento durante la eliminación del mismo.");
-        }
-    }
-
-    /**
-     * Método para recuperar el controlador de la clase DiscountsMgt.
-     *
-     * @return El controlador de la clase DiscountsMgt.
-     */
-    public static DiscountsMgt getController() {
-        if (controller == null)
-            controller = new DiscountsMgt();
-        return controller;
+        deleteQuery.setInt(1, discount.getId());
+        
+        // Ejecutamos la setencia sql.
+        if (deleteQuery.execute() == false)
+            throw new ManagerErrorException("No se ha encontrado el descuento durante la eliminación del mismo.");
     }
 }
