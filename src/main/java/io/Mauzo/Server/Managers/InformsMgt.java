28afa1f88fcfe9d561629e5558f09665be19081b
Ant04X <50796747+ant04x@users.noreply.github.com>
2020-05-22 19:17:55 +0200

    Server - Throw redundante quitado.

--- src/main/java/io/Mauzo/Server/Managers/InformsMgt.java
+++ src/main/java/io/Mauzo/Server/Managers/InformsMgt.java
@@ -69,7 +69,7 @@ public class InformsMgt implements ManagersIntf<Inform> {
             }
         } finally {
             conn.setAutoCommit(true);
-            
+
         }
 
         return inform;
